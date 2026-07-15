e711fbe5d8cc39e295ecfc70ea8434c1f5fad4d3
egoist945402376 <egoist945402376@gmail.com>
2026-07-15 13:46:49 +0100

    replication

--- diffusion_code/load_and_run.py
+++ diffusion_code/load_and_run.py
@@ -1,43 +1,28 @@
-import torch
-from torch.utils.data import DataLoader, TensorDataset
-import matplotlib.pyplot as plt
 import numpy as np
+import torch
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
 
+from main import rectangle_data
 from model import NoiseScheduler, ForwardProcess, BackwardProcess, DiffusionModel
 from pac_bayes import compute_bound
 
 
-def rectangle_data(num_samples):
-    rng = np.random.default_rng()
-    x = rng.uniform(-1, 1, num_samples)
-    y = rng.uniform(-1, 1, num_samples)
-    X = np.stack((x, y), axis=1)
-    # X *= 4
-    return TensorDataset(torch.from_numpy(X.astype(np.float32)))
-
-
 if __name__ == '__main__':
-
-    # the model
+    # rebuild the exact same architecture used in main.py, then load the trained weights
     ns = NoiseScheduler(timesteps=1000, beta_start=1e-4, beta_end=0.02, beta_schedule='linear')
     bp = BackwardProcess(hidden_layers=3, hidden_dim=128, embed_size=128, time_embed_type='sinusoidal', input_embed_type='sinusoidal')
+    bp.load_state_dict(torch.load('backward_process.pt'))
     fp = ForwardProcess(noise_scheduler=ns)
     diff_model = DiffusionModel(forward_process=fp, backward_process=bp)
 
-    # the data
-    d = rectangle_data(num_samples=50000)
-    dl = dataloader = DataLoader(d.tensors[0], batch_size=100, shuffle=True)
-
-    # training
-    diff_model.train_model(train_loader=dl, epochs=500, lr=1e-4)
-    torch.save(diff_model.backward_process.state_dict(), 'backward_process.pt')
-
-    # compute bound
+    # bound
     bound_data = rectangle_data(num_samples=5000)
     bound_loader = DataLoader(bound_data.tensors[0], batch_size=100, shuffle=True)
     bound = compute_bound(data_loader=bound_loader, diff_model=diff_model, diameter=np.sqrt(8), lamda=5000, delta=0.05, dim=2)
+    print('Bound value: ', bound)
 
-    # show samples and originals
+    # plots
     plt.figure()
     real_samples = rectangle_data(num_samples=2000)
     plt.scatter(x=real_samples.tensors[0][:, 0], y=real_samples.tensors[0][:, 1], alpha=0.5)
@@ -50,9 +35,4 @@ if __name__ == '__main__':
     plt.title('Fake samples')
     plt.savefig('fake_samples.png')
 
-    print('Bound value: ', bound)
     plt.show()
-
-
-
-
